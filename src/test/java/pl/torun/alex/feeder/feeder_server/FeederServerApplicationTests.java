1cfd43061f4e0f095860803c1e47ad2766aa9ff5
alex <sashayassa@gmail.com>
2025-10-26 12:11:08 +0100

    create project

---
+++ src/test/java/pl/torun/alex/feeder/feeder_server/FeederServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.torun.alex.feeder.feeder_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
