127b92f6cbee2814103fd93bb7ed536d6800dfeb
alex <sashayassa@gmail.com>
2025-11-08 16:02:57 +0100

    add db

---
+++ src/main/java/pl/torun/alex/feeder/feeder_server/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package pl.torun.alex.feeder.feeder_server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.torun.alex.feeder.feeder_server.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
